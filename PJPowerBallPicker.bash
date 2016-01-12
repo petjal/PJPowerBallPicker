8d7365d2df552f464a2c6f77335bd333c84d8993
E.W. Peter Jalajas <lists@tebuco.com>
2016-01-12 00:59:25 -0500

    Update PJPowerBallPicker.bash

--- PJPowerBallPicker.bash
+++ PJPowerBallPicker.bash
@@ -10,6 +10,17 @@
 
 #USAGE: for mtix in {001..100} true ; do ./PJPowerBallPicker.bash ; done
 
+#OUTPUT: like:
+#2 14 20 39 67 / 18
+#1 6 53 65 69 / 18
+#16 42 62 65 67 / 9
+#8 22 31 47 69 / 17
+#37 43 54 62 63 / 24
+#9 18 33 37 42 / 15
+#7 14 42 50 65 / 9
+#1 8 16 18 25 / 22
+#9 19 28 34 49 / 23
+
 #CREDITS:  
 #Inspired by: https://www.random.org/quick-pick/?tickets=100&lottery=5x69.1x26
 #Stolen from web:  od /dev/urandom , tr|sort, echo | wc 
@@ -24,25 +35,33 @@ mpicks=0
 mticket=''  # will be like "09 12 27 35 58 / 22"
 
 #MAIN
+#Select the white balls
 while true
 do
+    #Gather random bis from urandam and convert to digits
     mball=" "$(od -A n -t d -N 1 /dev/urandom)" "
+    #Test if in range, and if not already selected.  BUG in there somewhere.
     if [ "$mball" -ge "$mAmin" ] && \
        [ "$mball" -le "$mAmax" ] && \
        [[ $mticket != .*\s${mball}\ .* ]] ; then
+       #New number, so append to itcket
        mticket=" $mticket $mball "
+       #Sort the ticket numbers and remove any accidental duplicates
        mticket=$(echo $mticket | tr " " "\n" | sort -nu | tr "\n" " " )
        #echo $mticket
     fi
 
+    #Count how many unique numbers we have
     mpicks=$(echo $mticket | wc -w)
 
+    #If we have enough numbers for a ticket break out of the loop
     if [ "$mpicks" == "$mAnum" ] ; then
         break
     fi
     #echo $mticket
 done
 
+#Select the powerball
 while true
 do
     mball=" "$(od -A n -t d -N 1 /dev/urandom)" "
@@ -54,6 +73,9 @@ do
     fi
 done
 
+#Append the powerball to the ticket string
 mticket=$(echo $mticket "/" $mpowerball )
+
+#Print out the ticket
 echo $mticket
 
